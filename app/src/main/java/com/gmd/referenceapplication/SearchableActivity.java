fa68ae5d13c1ef49de2d93bc032f2877c191f5fc
Grace <gracedorgan@gmail.com>
2016-07-09 10:47:32 -0600

    Update

--- app/src/main/java/com/gmd/referenceapplication/SearchableActivity.java
+++ app/src/main/java/com/gmd/referenceapplication/SearchableActivity.java
@@ -21,10 +21,13 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class SearchableActivity extends AppCompatActivity {
 
     DatabaseTable db= new DatabaseTable(this);
 
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,57 +40,15 @@ public class SearchableActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        mListView = (ListView)findViewById(R.id.list);
 
-
+        Log.d("Search", "Getting in here");
 //        get the intent sent when user searches from search widget, verify the action and extract what is typed in
         Intent intent = getIntent();
         handleIntent(intent);
-
-        }
-
-    //creates menu
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.overflow, menu);
-
-
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //search button is pressed
-                String[] colums = new String[]{"QUANTITY"};
-                Cursor c=db.getWordMatches(query,colums);
-                Log.e("cursor returned",String.valueOf(c));
-                doSearch(c);
-
-                Log.e("searchable activity","text submitted");
-
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // User changed the text
-                Cursor c=db.getWordMatches(newText,null);
-                doSearch(c);
-
-                Log.e("searchable activity ", "haha losing will to live");
-                return true;
-            }
-        });
-
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-
-        return true;
-
     }
+
+
 
 
 
@@ -104,8 +65,6 @@ public class SearchableActivity extends AppCompatActivity {
             doSearch(c);
             Log.e("Search Operation", "Database searched");
 
-
-            //still need to process Cursor and display results
         }
     }
 
@@ -123,15 +82,13 @@ public class SearchableActivity extends AppCompatActivity {
         startManagingCursor(c);
 
 
-        ListView sl =(ListView)findViewById(android.R.id.list);
-
         String[] from = new String[] {"QUANTITY", "_id"};
         int[] to = new int[] {android.R.id.text1};
         SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, c, from, to);
-        sl.setAdapter(cursorAdapter);
+        mListView.setAdapter(cursorAdapter);
         Log.e("doSearch method:", "has been called");
 
-        sl.setOnItemClickListener(
+        mListView.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     public void onItemClick(AdapterView<?> parent, View view,
                                             int position, long id) {
@@ -140,52 +97,4 @@ public class SearchableActivity extends AppCompatActivity {
                     }
                 });
     }
-
-
-
-    //manages activity bar
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.constants:
-                startActivity(new Intent(this, FundamentalPhysicalConstants.class));
-                return true;
-
-            case R.id.joes_rules:
-                //go to rules
-                //startActivity(new Intent(MainActivity.this, ExampleListView.class));
-                return true;
-
-            case R.id.home:
-                //Go back to the home screen
-                startActivity(new Intent(this, MainActivity.class));
-                return true;
-
-            case R.id.search:
-                //open search
-                //startActivity(new Intent(this, SearchableActivity.class));
-                return true;
-
-            case R.id.links:
-                //go to referencelinks
-                startActivity(new Intent(this, ReferenceLinks.class));
-                return true;
-
-            case R.id.base_units:
-                //go to baseunits
-                startActivity(new Intent(this, SIBaseUnits.class));
-                return true;
-
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
-
-
-
 }
